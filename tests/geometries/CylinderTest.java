9890ac22d9a6ef649d8bc41e24fd09b3dc9f04e7
Elad <fisherelad3@gmail.com>
2021-04-07 23:45:31 +0300

    Ex2WithCylinderBonus

--- tests/geometries/CylinderTest.java
+++ tests/geometries/CylinderTest.java
@@ -15,22 +15,22 @@ class CylinderTest {
     @Test
     void testGetNormal() {
 
-        Point3D p0 = new Point3D(0,0,0);
-        Vector vector = new Vector(0,0,2);
-        double r = 1, h= 8;
+        Point3D p0 = new Point3D(0, 0, 0);
+        Vector vector = new Vector(0, 0, 2);
+        double r = 1, h = 8;
 
-        Cylinder cylinder  = new Cylinder(new Ray(p0,vector),r,h);
+        Cylinder cylinder = new Cylinder(new Ray(p0, vector), r, h);
 
         // ============ Equivalence Partitions Tests ==============
 
-        Vector upV = new Vector(0,0,1);
-        Point3D upP = new Point3D(0.38,-0.43,8);
+        Vector upV = new Vector(0, 0, 1);
+        Point3D upP = new Point3D(0.38, -0.43, 8);
 
-        Vector downV = new Vector(0,0,-1);
-        Point3D downP = new Point3D(0.38,-0.43,0);
+        Vector downV = new Vector(0, 0, -1);
+        Point3D downP = new Point3D(0.38, -0.43, 0);
 
-        Vector middleV = new Vector(0,1,0);
-        Point3D middleP = new Point3D(0,1,2);
+        Vector middleV = new Vector(0, 1, 0);
+        Point3D middleP = new Point3D(0, 1, 2);
 
         // TC01: point is in the far flat side of the ray origin
         assertTrue(cylinder.getNormal(upP).equals(upV),
@@ -46,17 +46,16 @@ class CylinderTest {
 
         // =============== Boundary Values Tests ==================
         // TC01: point is in the middle of the far flat side of  the ray origin
-        Vector middleUpV = new Vector(0,0,1);
-        Point3D middleUpP = new Point3D(0,0,8);
+        Vector middleUpV = new Vector(0, 0, 1);
+        Point3D middleUpP = new Point3D(0, 0, 8);
         assertTrue(cylinder.getNormal(middleUpP).equals(middleUpV),
                    "ERROR: wrong normal vector when the point is on the flat side away from the central point in the middle");
 
         // TC02: point is in the middle of the near flat side of the ray origin
-        Vector middleDownV = new Vector(0,0,-1);
-        Point3D middleDownP = new Point3D(0,0,0);
+        Vector middleDownV = new Vector(0, 0, -1);
+        Point3D middleDownP = new Point3D(0, 0, 0);
         assertTrue(cylinder.getNormal(middleDownP).equals(middleDownV),
                    "ERROR: wrong normal vector when the point is on the flat side near the central point in the middle");
-
 
 
     }
