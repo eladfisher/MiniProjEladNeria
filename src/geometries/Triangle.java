849c90e124b6d2686823da6ca552080810d618c0
Elad <fisherelad3@gmail.com>
2021-04-18 16:27:18 +0300

    tests

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -1,14 +1,24 @@
 package geometries;
 
 import primitives.Point3D;
+import primitives.Ray;
+
+import java.util.List;
 
 /**
  * a class of Triangle for a 3D graphic model
  */
-public class Triangle extends Plane{
-
-    public  Triangle(Point3D p0, Point3D p1 , Point3D p2){
-        super(p0,p1,p2);
-    }
-
+public class Triangle extends Plane {
+	
+	public Triangle(Point3D p0, Point3D p1, Point3D p2) {
+		super(p0, p1, p2);
+	}
+	
+	@Override
+	public List<Point3D> findIntersections(Ray ray) {
+		return null;
+	
+	}
+	
+	
 }
