7c46c1360e10b9fcd6e90fbfbbc5096377470462
נריה מחפוד <mahpod@g.jct.ac.il>
2021-06-01 13:10:25 +0300

    rotate camera and lookAt point and move to.

--- src/elements/Camera.java
+++ src/elements/Camera.java
@@ -1,7 +1,6 @@
 package elements;
 
 import primitives.*;
-
 import static primitives.Util.*;
 
 /**
@@ -9,41 +8,29 @@ import static primitives.Util.*;
  * the use of this class is to use camera in order to render a scene
  */
 public class Camera {
-	private final Point3D point3D;
-	
-	private final Vector vTo;
-	private final Vector vUp;
-	private final  Vector vRight;
-	
+	private Point3D point3D;
+
+	private Vector vTo;
+	private Vector vUp;
+	private Vector vRight;
+
 	private double height;
 	private double width;
 	private double distance;
-	
+
 	/**
 	 * @param point3D the point where the Camera is
 	 * @param vTo     the direction where the camera looking
 	 * @param vUp     the direction that up relate to the camera
 	 */
 	public Camera(Point3D point3D, Vector vTo, Vector vUp) {
-		
-		//
-		if (!isZero(vTo.dotProduct(vUp)))
-		{
-			throw new IllegalArgumentException("vTo have to be orthogonal to vUp");
-		}
-		
+		set_vTo_vUp(vTo,vUp);
 		this.point3D = point3D;
-		this.vTo = vTo.normalized();
-		this.vUp = vUp.normalized();
-		
-		vRight = vTo.crossProduct(vUp).normalize();
-		
-		
 	}
-	
-	
+
+
 	//region Getters
-	
+
 	/**
 	 * getter for the point field
 	 *
@@ -52,7 +39,7 @@ public class Camera {
 	public Point3D getPoint3D() {
 		return point3D;
 	}
-	
+
 	/**
 	 * getter for the point field
 	 *
@@ -61,7 +48,7 @@ public class Camera {
 	public Vector getvTo() {
 		return vTo;
 	}
-	
+
 	/**
 	 * getter for the up vector field
 	 *
@@ -70,7 +57,7 @@ public class Camera {
 	public Vector getvUp() {
 		return vUp;
 	}
-	
+
 	/**
 	 * getter for the right vector field
 	 *
@@ -79,7 +66,7 @@ public class Camera {
 	public Vector getvRight() {
 		return vRight;
 	}
-	
+
 	/**
 	 * getter for the Height field
 	 *
@@ -88,7 +75,7 @@ public class Camera {
 	public double getHeight() {
 		return height;
 	}
-	
+
 	/**
 	 * getter for the Width field
 	 *
@@ -97,7 +84,7 @@ public class Camera {
 	public double getWidth() {
 		return width;
 	}
-	
+
 	/**
 	 * getter for the distance field
 	 *
@@ -106,13 +93,108 @@ public class Camera {
 	public double getDistance() {
 		return distance;
 	}
-	
-	
+
+
 	//endregion
-	
-	
+
+	//region Setters
+
+	/**
+	 *
+	 * @param vTo
+	 * @param vUp
+	 */
+	public void set_vTo_vUp(Vector vTo, Vector vUp){
+		if (!isZero(vTo.dotProduct(vUp)))
+		{
+			throw new IllegalArgumentException("vTo have to be orthogonal to vUp");
+		}
+
+		this.vTo = vTo.normalized();
+		this.vUp = vUp.normalized();
+
+		vRight = vTo.crossProduct(vUp).normalize();
+	}
+
+	/**
+	 *
+	 * @param p
+	 */
+	public void setPoint3D(Point3D p)
+	{
+		point3D = p;
+	}
+
+	/**
+	 *
+	 * @param p
+	 * @param axisUp
+	 */
+	public void lookAt(Point3D p, Vector axisUp){
+		Vector to = p.subtract(point3D).normalized();
+		axisUp.normalize();
+
+		if (isZero(to.dotProduct(axisUp)))
+			set_vTo_vUp(to, axisUp);
+
+		if (axisUp.equals(to))
+			throw new IllegalArgumentException("axisUp points at the camera.");
+
+		Vector h = to.scale(-1);
+		double cos_a = h.dotProduct(axisUp)/(h.length()*axisUp.length());
+		double b = p.distance(point3D);
+
+		double d = b/cos_a;
+
+		Point3D pup = p.add(axisUp.scale(d));
+
+		Vector up = pup.subtract(point3D).normalized();
+
+		set_vTo_vUp(to,up);
+	}
+
+    /**
+     *
+     * @param a
+     */
+	public void rotateCameraAroundVto(double a){
+		Point3D h0 = Point3D.ZERO;
+		Vector ToH0 = h0.subtract(point3D);
+		Point3D Hup = point3D.add(vUp);
+		Hup = Hup.add(ToH0);
+
+		Matrix v = new Matrix(Hup);
+		double x = vTo.getHead().getX(),
+				y = vTo.getHead().getY(),
+				z = vTo.getHead().getZ(),
+				ca = Math.cos(a),
+				sa = Math.sin(a);
+		double[][] ra = {
+				{ca+x*x*(1-ca),		x*y*(1-ca)-z*sa,	x*z*(1-ca)+y*sa},
+				{x*y*(1-ca)+z*sa,	ca+y*y*(1-ca),		z*y*(1-ca)-x*sa},
+				{x*z*(1-ca)-y*sa,	z*y*(1-ca)+x*sa,	ca+z*z*(1-ca)}
+		};
+
+		Matrix rm = new Matrix(ra);
+
+		Matrix rotate = rm.times(v);
+
+		double[][] drot = rotate.getData();
+		Point3D newone = new Point3D(drot[0][0],drot[1][0],drot[2][0]);
+
+		set_vTo_vUp(vTo,newone.subtract(h0));
+	}
+
+	public void MoveCamera(Point3D newPlace, Point3D lookAtP, double a)
+    {
+        point3D = newPlace;
+        lookAt(lookAtP,new Vector(0,1,0));
+        rotateCameraAroundVto(a);
+    }
+	//endregion
+
 	//region getters using method chaining
-	
+
 	/**
 	 * update the size of the view plane
 	 *
@@ -123,10 +205,10 @@ public class Camera {
 	public Camera setVpSize(double width, double height) {
 		this.width = width;
 		this.height = height;
-		
+
 		return this;
 	}
-	
+
 	/**
 	 * update the distance of the camera from view plane
 	 *
@@ -135,10 +217,10 @@ public class Camera {
 	 */
 	public Camera setVpDistance(double distance) {
 		this.distance = distance;
-		
+
 		return this;
 	}
-	
+
 	/**
 	 * construct a ray Through a specific Pixel on the view plane
 	 * and return the ray
@@ -152,32 +234,32 @@ public class Camera {
 	public Ray constructRayThroughPixel(int nX, int nY, int j, int i) {
 		Point3D p0 = this.point3D;
 		Point3D pC = p0.add(vTo.scale(distance));// find the center of the view plane
-		
+
 		double ratioY = height / nY;//the ratio between the height and the number of pixels in each column
 		double ratioX = width / nX;//the ratio between the width and the number of pixels in each row
-		
+
 		double nxJ = (double) (nX - 1) / 2;
 		double xJ = (j - nxJ) * ratioX;//the x coordinate of the pixel
-		
+
 		double nyI = (double) (nY - 1) / 2;
 		double yI = -(i - nyI) * ratioY;//the  coordinate of the pixel
-		
+
 		Point3D Pij = pC;//.add(vRight.scale(xJ)).add(vUp.scale(yI))
-		
-		
+
+
 		//without this if statement an exception will be thrown
 		//because vector 0 will be created in case that either of xJ or yI will be 0
 		if (xJ != 0)// if there is a need to change the x axis point
 			Pij = Pij.add(vRight.scale(xJ));
-		
+
 		if (yI != 0)// if there is a need to change the y axis point
 			Pij = Pij.add(vUp.scale(yI));
-		
+
 		return new Ray(p0, Pij.subtract(p0));
 	}
-	
-	
+
+
 	//endregion
-	
-	
+
+
 }
--- tests/elements/CameraTest.java
+++ tests/elements/CameraTest.java
@@ -1,7 +1,14 @@
 package elements;
 
+import geometries.Plane;
+import geometries.Polygon;
+import geometries.Sphere;
 import org.junit.jupiter.api.Test;
 import primitives.*;
+import renderer.ImageWriter;
+import renderer.RayTracerBasic;
+import renderer.Render;
+import scene.Scene;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,48 +17,297 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Testing Camera Class
  *
  * @author Dan
- *
  */
 public class CameraTest {
-	
-	/**
-	 * Test method for
-	 * {@link elements.Camera#constructRayThroughPixel(int, int, int, int)}.
-	 */
-	@Test
-	public void testConstructRayThroughPixel() {
-		Camera camera = new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0)).setVpDistance(10);
-		
-		// ============ Equivalence Partitions Tests ==============
-		// TC01: 3X3 Corner (0,0)
-		assertEquals(new Ray(Point3D.ZERO, new Vector(-2, -2, 10)),
-					 camera.setVpSize(6, 6).constructRayThroughPixel(3, 3, 0, 0),"Bad ray");
-		
-		// TC02: 4X4 Corner (0,0)
-		assertEquals( new Ray(Point3D.ZERO, new Vector(-3, -3, 10)),
-					 camera.setVpSize(8, 8).constructRayThroughPixel(4, 4, 0, 0),"Bad ray");
-		
-		// TC03: 4X4 Side (0,1)
-		assertEquals( new Ray(Point3D.ZERO, new Vector(-1, -3, 10)),
-					 camera.setVpSize(8, 8).constructRayThroughPixel(4, 4, 1, 0),"Bad ray");
-		
-		// TC04: 4X4 Inside (1,1)
-		assertEquals( new Ray(Point3D.ZERO, new Vector(-1, -1, 10)),
-					 camera.setVpSize(8, 8).constructRayThroughPixel(4, 4, 1, 1),"Bad ray");
-		
-		// =============== Boundary Values Tests ==================
-		// TC11: 3X3 Center (1,1)
-		assertEquals( new Ray(Point3D.ZERO, new Vector(0, 0, 10)),
-					 camera.setVpSize(6, 6).constructRayThroughPixel(3, 3, 1, 1),"Bad ray");
-		
-		// TC12: 3X3 Center of Upper Side (0,1)
-		assertEquals( new Ray(Point3D.ZERO, new Vector(0, -2, 10)),
-					 camera.setVpSize(6, 6).constructRayThroughPixel(3, 3, 1, 0),"Bad ray");
-		
-		// TC13: 3X3 Center of Left Side (1,0)
-		assertEquals( new Ray(Point3D.ZERO, new Vector(-2, 0, 10)),
-					 camera.setVpSize(6, 6).constructRayThroughPixel(3, 3, 0, 1),"Bad ray");
-		
-	}
-	
+
+    /**
+     * Test method for
+     * {@link elements.Camera#constructRayThroughPixel(int, int, int, int)}.
+     */
+    @Test
+    public void testConstructRayThroughPixel() {
+        Camera camera = new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0)).setVpDistance(10);
+
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: 3X3 Corner (0,0)
+        assertEquals(new Ray(Point3D.ZERO, new Vector(-2, -2, 10)),
+                camera.setVpSize(6, 6).constructRayThroughPixel(3, 3, 0, 0), "Bad ray");
+
+        // TC02: 4X4 Corner (0,0)
+        assertEquals(new Ray(Point3D.ZERO, new Vector(-3, -3, 10)),
+                camera.setVpSize(8, 8).constructRayThroughPixel(4, 4, 0, 0), "Bad ray");
+
+        // TC03: 4X4 Side (0,1)
+        assertEquals(new Ray(Point3D.ZERO, new Vector(-1, -3, 10)),
+                camera.setVpSize(8, 8).constructRayThroughPixel(4, 4, 1, 0), "Bad ray");
+
+        // TC04: 4X4 Inside (1,1)
+        assertEquals(new Ray(Point3D.ZERO, new Vector(-1, -1, 10)),
+                camera.setVpSize(8, 8).constructRayThroughPixel(4, 4, 1, 1), "Bad ray");
+
+        // =============== Boundary Values Tests ==================
+        // TC11: 3X3 Center (1,1)
+        assertEquals(new Ray(Point3D.ZERO, new Vector(0, 0, 10)),
+                camera.setVpSize(6, 6).constructRayThroughPixel(3, 3, 1, 1), "Bad ray");
+
+        // TC12: 3X3 Center of Upper Side (0,1)
+        assertEquals(new Ray(Point3D.ZERO, new Vector(0, -2, 10)),
+                camera.setVpSize(6, 6).constructRayThroughPixel(3, 3, 1, 0), "Bad ray");
+
+        // TC13: 3X3 Center of Left Side (1,0)
+        assertEquals(new Ray(Point3D.ZERO, new Vector(-2, 0, 10)),
+                camera.setVpSize(6, 6).constructRayThroughPixel(3, 3, 0, 1), "Bad ray");
+
+    }
+
+    @Test
+    void lookAt() {
+        Scene scene = new Scene("Test scene");
+
+        Material wall_m = new Material().setKd(1).setKs(1).setShininess(30).setKt(0);
+        Color wall_c = new Color(java.awt.Color.gray);
+        double d = 20;
+        Polygon r_wall = (Polygon) new Polygon(
+                new Point3D(d * 2, d * 0, d * 1),
+                new Point3D(d * 2, d * 0, d * -2),
+                new Point3D(d * 2, d * 4, d * -2),
+                new Point3D(d * 2, d * 4, d * 1)
+        ).setEmission(wall_c)
+                .setMaterial(wall_m);
+        Polygon b_wall = (Polygon) new Polygon(
+                new Point3D(d * -2, d * 0, d * -2),
+                new Point3D(d * 2, d * 0, d * -2),
+                new Point3D(d * 2, d * 4, d * -2),
+                new Point3D(d * 0, d * 6, d * -2),
+                new Point3D(d * -2, d * 4, d * -2)
+        ).setEmission(wall_c)
+                .setMaterial(wall_m);
+        Polygon l_wall = (Polygon) new Polygon(
+                new Point3D(d * -2, d * 0, d * 1),
+                new Point3D(d * -2, d * 0, d * -2),
+                new Point3D(d * -2, d * 4, d * -2),
+                new Point3D(d * -2, d * 4, d * 1)
+        ).setEmission(wall_c)
+                .setMaterial(wall_m);
+        Polygon f_wall = (Polygon) new Polygon(
+                new Point3D(d * -2, d * 0, d * 1),
+                new Point3D(d * 2, d * 0, d * 1),
+                new Point3D(d * 2, d * 4, d * 1),
+                new Point3D(d * 0, d * 6, d * 1),
+                new Point3D(d * -2, d * 4, d * 1)
+        ).setEmission(wall_c)
+                .setMaterial(wall_m);
+
+
+        Material roof_m = new Material().setKd(0.4).setKs(0.1).setShininess(10).setKt(0);
+        Color roof_c = new Color(java.awt.Color.RED);
+        Polygon r_roof = (Polygon) new Polygon(
+                new Point3D(d * 0, d * 6, d * 1),
+                new Point3D(d * 0, d * 6, d * -2),
+                new Point3D(d * 2.5, d * 3.5, d * -2),
+                new Point3D(d * 2.5, d * 3.5, d * 1)
+        ).setEmission(roof_c)
+                .setMaterial(roof_m);
+        Polygon l_roof = (Polygon) new Polygon(
+                new Point3D(d * 0, d * 6, d * 1),
+                new Point3D(d * 0, d * 6, d * -2),
+                new Point3D(d * -2.5, d * 3.5, d * -2),
+                new Point3D(d * -2.5, d * 3.5, d * 1)
+        ).setEmission(roof_c)
+                .setMaterial(roof_m);
+
+        Polygon door = (Polygon) new Polygon(
+                new Point3D(d * -0.7, d * 0, d * 1.005),
+                new Point3D(d * 0.7, d * 0, d * 1.005),
+                new Point3D(d * 0.7, d * 2, d * 1.005),
+                new Point3D(d * -0.7, d * 2, d * 1.005)
+        ).setEmission(new Color(139, 69, 19))
+                .setMaterial(wall_m);
+
+        Material ground_m = new Material().setKd(0.3).setKs(0).setShininess(0).setKt(0).setKr(0.3);
+        Color ground_c = new Color(java.awt.Color.green);
+        Plane ground = (Plane) new Plane(new Vector(0, 1, 0), new Point3D(0, 0, 0))
+                .setMaterial(ground_m)
+                .setEmission(ground_c);
+
+
+        Point3D sun_p = new Point3D(-390, 240, -190);
+        Sphere sun = (Sphere) new Sphere(sun_p, 70)
+                .setEmission(new Color(java.awt.Color.yellow))
+                .setMaterial(new Material()
+                        .setKd(0.2)
+                        .setKt(0.9)
+                        .setShininess(100)
+                        .setKs(1));
+
+        scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
+        scene.setBackground(new Color(java.awt.Color.CYAN).scale(0.7));
+        scene.geometries.add(
+                r_wall, l_wall, b_wall, f_wall,
+                door,
+                r_roof, l_roof,
+                ground,
+                sun
+        );
+
+        scene.lights.add(new DirectionalLight(
+                new Color(java.awt.Color.WHITE).scale(0.5),
+                new Vector(1, -1, -1)
+        ));
+        scene.lights.add(
+                new PointLight(new Color(java.awt.Color.WHITE), sun_p)
+        );
+
+
+        Camera coolCamera = new Camera(new Point3D(1000, 150, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+                .setVpSize(200, 200).setVpDistance(1000);
+
+        ImageWriter imageWriter = new ImageWriter("testLookAtBeforeImage", 600, 600);
+        Render render = new Render() //
+                .setImageWriter(imageWriter) //
+                .setCamera(coolCamera) //
+                .setRayTracer(new RayTracerBasic(scene));
+
+        render.renderImage();
+        render.writeToImage();
+
+
+        coolCamera.lookAt(new Point3D(0,50,0), new Vector(0,1,0));
+        imageWriter = new ImageWriter("testLookAtAfterImage", 600, 600);
+        render = new Render() //
+                .setImageWriter(imageWriter) //
+                .setCamera(coolCamera) //
+                .setRayTracer(new RayTracerBasic(scene));
+
+        render.renderImage();
+        render.writeToImage();
+    }
+
+    @Test
+    void rotateCameraAroundVto() {
+        Scene scene = new Scene("Test scene");
+
+        Material wall_m = new Material().setKd(1).setKs(1).setShininess(30).setKt(0);
+        Color wall_c = new Color(java.awt.Color.gray);
+        double d = 20;
+        Polygon r_wall = (Polygon) new Polygon(
+                new Point3D(d * 2, d * 0, d * 1),
+                new Point3D(d * 2, d * 0, d * -2),
+                new Point3D(d * 2, d * 4, d * -2),
+                new Point3D(d * 2, d * 4, d * 1)
+        ).setEmission(wall_c)
+                .setMaterial(wall_m);
+        Polygon b_wall = (Polygon) new Polygon(
+                new Point3D(d * -2, d * 0, d * -2),
+                new Point3D(d * 2, d * 0, d * -2),
+                new Point3D(d * 2, d * 4, d * -2),
+                new Point3D(d * 0, d * 6, d * -2),
+                new Point3D(d * -2, d * 4, d * -2)
+        ).setEmission(wall_c)
+                .setMaterial(wall_m);
+        Polygon l_wall = (Polygon) new Polygon(
+                new Point3D(d * -2, d * 0, d * 1),
+                new Point3D(d * -2, d * 0, d * -2),
+                new Point3D(d * -2, d * 4, d * -2),
+                new Point3D(d * -2, d * 4, d * 1)
+        ).setEmission(wall_c)
+                .setMaterial(wall_m);
+        Polygon f_wall = (Polygon) new Polygon(
+                new Point3D(d * -2, d * 0, d * 1),
+                new Point3D(d * 2, d * 0, d * 1),
+                new Point3D(d * 2, d * 4, d * 1),
+                new Point3D(d * 0, d * 6, d * 1),
+                new Point3D(d * -2, d * 4, d * 1)
+        ).setEmission(wall_c)
+                .setMaterial(wall_m);
+
+
+        Material roof_m = new Material().setKd(0.4).setKs(0.1).setShininess(10).setKt(0);
+        Color roof_c = new Color(java.awt.Color.RED);
+        Polygon r_roof = (Polygon) new Polygon(
+                new Point3D(d * 0, d * 6, d * 1),
+                new Point3D(d * 0, d * 6, d * -2),
+                new Point3D(d * 2.5, d * 3.5, d * -2),
+                new Point3D(d * 2.5, d * 3.5, d * 1)
+        ).setEmission(roof_c)
+                .setMaterial(roof_m);
+        Polygon l_roof = (Polygon) new Polygon(
+                new Point3D(d * 0, d * 6, d * 1),
+                new Point3D(d * 0, d * 6, d * -2),
+                new Point3D(d * -2.5, d * 3.5, d * -2),
+                new Point3D(d * -2.5, d * 3.5, d * 1)
+        ).setEmission(roof_c)
+                .setMaterial(roof_m);
+
+        Polygon door = (Polygon) new Polygon(
+                new Point3D(d * -0.7, d * 0, d * 1.005),
+                new Point3D(d * 0.7, d * 0, d * 1.005),
+                new Point3D(d * 0.7, d * 2, d * 1.005),
+                new Point3D(d * -0.7, d * 2, d * 1.005)
+        ).setEmission(new Color(139, 69, 19))
+                .setMaterial(wall_m);
+
+        Material ground_m = new Material().setKd(0.3).setKs(0).setShininess(0).setKt(0).setKr(0.3);
+        Color ground_c = new Color(java.awt.Color.green);
+        Plane ground = (Plane) new Plane(new Vector(0, 1, 0), new Point3D(0, 0, 0))
+                .setMaterial(ground_m)
+                .setEmission(ground_c);
+
+
+        Point3D sun_p = new Point3D(-390, 240, -190);
+        Sphere sun = (Sphere) new Sphere(sun_p, 70)
+                .setEmission(new Color(java.awt.Color.yellow))
+                .setMaterial(new Material()
+                        .setKd(0.2)
+                        .setKt(0.9)
+                        .setShininess(100)
+                        .setKs(1));
+
+        scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
+        scene.setBackground(new Color(java.awt.Color.CYAN).scale(0.7));
+        scene.geometries.add(
+                r_wall, l_wall, b_wall, f_wall,
+                door,
+                r_roof, l_roof,
+                ground,
+                sun
+        );
+
+        scene.lights.add(new DirectionalLight(
+                new Color(java.awt.Color.WHITE).scale(0.5),
+                new Vector(1, -1, -1)
+        ));
+        scene.lights.add(
+                new PointLight(new Color(java.awt.Color.WHITE), sun_p)
+        );
+
+
+        Camera coolCamera = new Camera(new Point3D(1000, 150, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+                .setVpSize(200, 200).setVpDistance(1000);
+        coolCamera.lookAt(new Point3D(0,50,0), new Vector(0,1,0));
+
+        ImageWriter imageWriter = new ImageWriter("testRotateBeforeImage", 600, 600);
+        Render render = new Render() //
+                .setImageWriter(imageWriter) //
+                .setCamera(coolCamera) //
+                .setRayTracer(new RayTracerBasic(scene));
+
+        render.renderImage();
+        render.writeToImage();
+
+
+        coolCamera.rotateCameraAroundVto(45);
+        imageWriter = new ImageWriter("testRotateAfterImage", 600, 600);
+        render = new Render() //
+                .setImageWriter(imageWriter) //
+                .setCamera(coolCamera) //
+                .setRayTracer(new RayTracerBasic(scene));
+
+        render.renderImage();
+        render.writeToImage();
+    }
+
+    @Test
+    void moveCamera() {
+    }
 }
